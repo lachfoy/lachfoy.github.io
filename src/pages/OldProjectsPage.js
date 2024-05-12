4b9ae9baf1c93987773c95b0d7da3ce8065f1bba
lachfoy <lachie.foy7@gmail.com>
2024-05-12 12:30:42 +1000

    cleanup uneccessary files

--- src/pages/OldProjectsPage.js
+++ src/pages/OldProjectsPage.js
@@ -3,7 +3,7 @@
 import image1 from '../assets/2dgame.png';
 import turretgame from '../assets/turretgame.png';
 import cooltilerendering from '../assets/cooltilerendering.png';
-import butchermania from '../assets/butchermania.png';
+// import butchermania from '../assets/butchermania.png';
 import unitySpells from '../assets/unityspells.png';
 import physics from '../assets/2dphysics.png';
 
@@ -20,7 +20,7 @@ export function OldProjectsPage() {
         title="Another 2D engine"
         paragraphs={[
           "Not really a game, this project just represents my ongoing exploration into game engine programming.",
-          "It's really an excuse for me to implement things like sprite batching, debug drawing, text rendering, audio, resource management. etc. and is a \'jumping off point\' for my future 2D projects.",
+          "It's really an excuse for me to implement things like sprite batching, debug drawing, text rendering, audio, resource management. etc. and is a 'jumping off point' for my future 2D projects.",
           "I also tried integrating Box2D and IMGui in this project."
         ]}
         image={image1}
@@ -36,7 +36,7 @@ export function OldProjectsPage() {
       <GalleryItem
         title="Unfinished kind of rogue like game"
         paragraphs={[
-          "Although I didn't get far with this project I was quite proud of the \'lighting\'.",
+          "Although I didn't get far with this project I was quite proud of the 'lighting'.",
           "I wanted the wall tiles to have a soft shadow around them, and I came up with the idea of rendering out a texture with a black pixel for each wall tile. Then I stretched this texture by the size of the tiles and let bilinear filtering do the blur effect for me.",
           "The per-tile lighting was done with a djikstra algorithm to find out how far each tile is away from a lightsource, and change their brightness accordingly.",
           "This project also featured some very basic map generation. I generated rooms with a kind of recursive partitioning, then attempted to carve corridors between them. Overall the maps turned out pretty bad, but it was kinda cool."
